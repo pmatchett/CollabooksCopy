f21a79dde2fcc0886af7fa1d8ee13b1b7aa0725d
Siobhan O'Dell <siobhan.odell@ucalgary.ca>
2020-04-08 13:58:34 -0600

    html placed into html, non-functional remove button added to bookshelf

--- public/index.js
+++ public/index.js
@@ -74,134 +74,41 @@ function loanHandler(identifier){
 
 /************* Page Navigation **************/
 
-/*****Changes the selected tab on the navbar*****/
+/**Changes to the selected tab on the navbar**/
 $(document).on('click','.nav li', function (e) {
     $(this).addClass('active').siblings().removeClass('active');
 
     if ($(this).text() === "Home") {
-
-      // TODO: figure out why map isn't working
-      $("#mainContent").html(`
-        <div id="map"></div>
-        <button id="testButton" class="btn btn-secondary" type="button">Add a marker</button>
-        `);
-        initMap();
-      $("#sidebarContent").html(`
-        <table class="table table-hover text-left" id="booksidebar">
-          <thead>
-            <tr>
-              <th>Books Around</th>
-            </tr>
-          </thead>
-          <tbody>
-          <!-- /******** Books Added Here ********/ -->
-          </tbody>
-        </table>
-        <!-- DEBUG:  -->
-        <button type="button" class="btn btn-secondary" name="populate" onclick="populateBooksAround()">Populate</button>
-        `);
+      $('.HomePage').show();
+      $('.BookshelfPage').hide();
+      $('.RequestsPage').hide();
+      $('.ProfilePage').hide();
+      initMap();
     }
     else if ($(this).text() === "Bookshelf") {
-      $("#mainContent").html(`
-
-        <!---------- Bookshelf Content ----------->
-        <h1> My Bookshelf </h1>
-        <table class="table table-hover" id="bookshelf">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author</th>
-              <th>ISBN</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-          <!-- /******** Books Added Here ********/ -->
-          </tbody>
-        </table>
-        <!-- DEBUG:  -->
-        <button type="button" class="btn btn-secondary" name="populate" onclick="populateShelf()">Populate</button>
-        `);
-
-      $("#sidebarContent").html(`
-        <!---------- Bookshelf Content ----------->
-        <h1> Add A Book </h1>
-        <form>
-          <div class="form-group row">
-            <label for="inputTitle" class="col-sm-3 col-form-label">Title</label>
-            <div class="col-sm-9">
-              <input id="inputTitle" type="text" class="form-control">
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <label for="inputAuthor" class="col-sm-3 col-form-label">Author</label>
-            <div class="col-sm-9">
-              <input id="inputAuthor" type="text" class="form-control">
-              <small id="authorHelpBlock" class="form-text text-muted">
-                Last name, First name
-              </small>
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <label for="inputGenre" class="col-sm-3 col-form-label">Genre</label>
-            <div class="col-sm-9">
-            <select id="inputGenre" class="form-control form-control-sm">
-              <option>Select Genre...</option>
-              <option>Fiction</option>
-              <option>Non-Fiction</option>
-            </select>
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <label for="inputDuration" class="col-sm-3 col-form-label">Duration</label>
-            <div class="col-sm-9">
-            <select id="inputDuration" class="form-control form-control-sm">
-              <option>Select Borrow Duration...</option>
-              <option>1 Week</option>
-              <option>2 Weeks</option>
-              <option>3 Weeks</option>
-            </select>
-            </div>
-          </div>
-
-          <div class="form-group row">
-            <label for="inputISBN" class="col-sm-3 col-form-label">ISBN</label>
-            <div class="col-sm-9">
-              <input id="inputISBN" type="number" class="form-control">
-            </div>
-          </div>
-
-          <button type="submit" class="btn btn-secondary btn-lg">Add</button>
-        </form>
-        `);
+      $('.HomePage').hide();
+      $('.BookshelfPage').show();
+      $('.RequestsPage').hide();
+      $('.ProfilePage').hide();
     }
     else if ($(this).text() === "Requests") {
-      $("#mainContent").html(`
-        <!---------- Chat Window ----------->
-        <ul id="messages"></ul>
-        <div class="row">
-            <div class="col-lg-12">
-                <form class="form-inline" action="">
-                <input type="text" id="message-box" autocomplete="off" class="form-control" placeholder="Enter your message"/>
-                <button type="button" id="send-button" class="btn btn-info">Send</button>
-                </form>
-            </div>
-        </div>
-        `);
-
-      $("#sidebarContent").html("Placeholder for where other chats go");
+      $('.HomePage').hide();
+      $('.BookshelfPage').hide();
+      $('.RequestsPage').show();
+      $('.ProfilePage').hide();
     }
     else if ($(this).text() === "My Profile") {
-      $("#mainContent").html("This is a Profile.");
-      $("#sidebarContent").html("This is a Sidebar.");
+      $('.HomePage').hide();
+      $('.BookshelfPage').hide();
+      $('.RequestsPage').hide();
+      $('.ProfilePage').show();
     }
 
 } );
 
-/************* DEBUG **************/
+/************* Placeholder Functions **************/
+
+// TODO: connect to database
 function populateShelf()
 {
   let books = [{title: "Twilight",author: "Meyer, Stephenie",ISBN: 7387258726782,status: "in"},
@@ -214,11 +121,13 @@ function populateShelf()
       "<td>" + item.title + "</td>" +
       "<td>" + item.author + "</td>" +
       "<td>" + item.ISBN + "</td>" +
-      "<td>" + item.status + "</td>"
+      "<td>" + item.status + "</td>" +
+      '<td><button type="button" class="btn btn-secondary" name="removeBook" onclick="removeBook(' + item.ISBN + ')">Remove</button></td>'
       ));
   });
 }
 
+// TODO: connect to database
 function populateBooksAround()
 {
   let books = [{title: "Twilight",author: "Meyer, Stephenie",ISBN: 7387258726782,status: "in", latitude:51.078113, longitude:-114.129029},
@@ -233,4 +142,9 @@ function populateBooksAround()
     addMarker(item);
   });
 
+}
+
+// TODO: connect to database
+function removeBook(removeISBN){
+  console.log("Removed " + removeISBN);
 }

d9046728c393e6c9662c16a1d51e1b7852aed859
Brandt <brandtd77@gmail.com>
2020-04-19 12:21:55 -0600

    Refactored code, and updated the values stored in cookie
    (cherry picked from commit 6685d7e5fc46c18d4ec7dc777fa0e3269cb04857)

--- public/login.js
+++ public/login.js
@@ -6,41 +6,40 @@
 *           Siobhan O’Dell
 *           Kent Wong
 *  Created On: 11/03/2020
-*  Last revision: 04/18/2020
+*  Last revision: 04/19/2020
 ********************************************/
 
 async function auth(){
         let inputEmail= document.getElementById("usernameInput").value;
         let inputPass = document.getElementById("passwordInput").value;
-        
-        // These variables will be utilized once I tidy up this
-        // code
-
-        // let userID = 
-        // let userEmail = 
-        // let userPass = 
-        // let userStatus = 
-     
         let newPage = document.getElementById("submit");
-        emailStatus = await checkEmail(inputEmail)
-        passwordStatus = await passwordMatches(inputEmail, inputPass)
-        activeStatus = await isActiveUser(inputEmail)
-
-        // Check if email exists
-        if(emailStatus === false){
-            console.log("Email failed")
+        
+        let record = await getRecord(inputEmail);
+        
+        // Determines if the record with the input email exists or not
+        if(record === false){
             loginFailed();
             return;
         }
+        
+        // It is probably terrible style to store this here
+        let userPass = record.password;  // Used for authentication
+        let userStatus = record.status;  // Used for authentication
+
+        let userID = record.user_id;     // Used for cookie
+        let userLat = record.user_lon;   // Used for cookie 
+        let userLon = record.user_lat;   // Used for cookie
+        let userType = record.user_type; // Used for cookie
+
         // Check if password matches email
-        else if(passwordStatus === false){
-            console.log("password failed")
+        if(userPass !== inputPass){
+            console.log("password failed");
             loginFailed();
             return;
         }
         // Check if user is blocked
-        else if(activeStatus === false){
-            console.log("active failed")
+        else if(userStatus !== "active"){
+            console.log("active failed");
             loginFailed();
             return;
         }
@@ -50,15 +49,14 @@ async function auth(){
         // and then I changed index.html to index1.html
         // ************************
         else {
-            alert("Login was successful, please click the button again to sign in")
-            await buildCookies(inputEmail)
-            console.log(document.cookie)
+            alert("Login was successful!\nPlease click the \"Sign In\" button again to complete sign in");
+            await buildCookies(userID, userLat, userLon, userType);
             newPage.setAttribute("href", "index1.html");
         }
 }
 
 async function loginFailed(){
-    alert("INCORRECT LOGIN INFORMATION\nPlease try again")
+    alert("INCORRECT LOGIN INFORMATION\nPlease try again");
     let newPage = document.getElementById("submit");
     newPage.setAttribute("href", "index.html");
 }
@@ -70,72 +68,15 @@ async function getRecord(inputEmail) {
 
     let result = await apiGetRecord(get_user_record);
     //console.log(result[0]);
-    //console.log(result[0].user_id)
-    return result[0]
+    if(result.length === 0){
+        return false;
+    }
+    return result[0];
 }
 
-async function buildCookies(inputEmail){
-    let record = await getRecord(inputEmail);
-    console.log(record)
-    
-    let userID = record.user_id;
-    let userLat = record.user_lon;
-    let userLon = record.user_lat;
-
+async function buildCookies(userID, userLat, userLon, userType){
     document.cookie = "user_id=" + userID + ";";
     document.cookie = "user_lat=" + userLat + ";";
     document.cookie = "user_lon=" + userLon + ";";
-}
-
-async function checkEmail(inputEmail) {
-    let get_user_record = {tablename: 'user_table', 
-                           column_name: 'email',
-                           value: inputEmail};
-
-    let result = await apiGetRecord(get_user_record);
-    console.log(result)
-    console.log(result.length)
-
-    if(result.length === 0){
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-
-async function passwordMatches(inputEmail, inputPass) {
-    let get_user_record = {tablename: 'user_table', 
-                           column_name: 'email',
-                           column_name: 'password',
-                           value: inputEmail, 
-                           value: inputPass  };
-
-    let result = await apiGetRecord(get_user_record);
-    console.log(result)
-
-    if(result.length === 0){
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-
-async function isActiveUser(inputEmail) {
-    let get_user_record = {tablename: 'user_table', 
-                           column_name: 'email',
-                           column_name: 'status',
-                           value: inputEmail, 
-                           value: 'active'  };
-
-    let result = await apiGetRecord(get_user_record);
-    console.log(result)
-
-    if(result.length === 0){
-        return false;
-    }
-    else {
-        return true;
-    }
+    document.cookie = "user_type=" + userType + ";";
 }

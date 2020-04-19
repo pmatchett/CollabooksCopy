3163f58d590c306179ac6c196777a92bc327af18
Brandt <brandtd77@gmail.com>
2020-04-19 14:31:44 -0600

    adding index changes and login js

--- public/login.js
+++ public/login.js
@@ -6,33 +6,108 @@
 *           Siobhan O’Dell
 *           Kent Wong
 *  Created On: 11/03/2020
-*  Last revision: 04/07/2020
+*  Last revision: 04/18/2020
 ********************************************/
 
-/** Required to use passport.js:
- * NOTE: passport is currently not being utilized, for now, I will
- * consider it a "nice to have"
- * npm install passport-google-oauth
- * 
- */
+async function auth(){
+        let inputEmail= document.getElementById("usernameInput").value;
+        let inputPass = document.getElementById("passwordInput").value;
+        
 
-// Hardcoded dict to store "existing users" 
-// This is just to ensure the authentication logic is valid
-// Later implentation will have DB queries and/or passport.js to communicate
-// with external data, etc.
-var users = {'BillyBob@hello.ca':1234, 'JaneDoe@hi.com':1234};
-
-
-function auth(){
-        let username = document.getElementById("usernameInput").value;
-        let password = document.getElementById("passwordInput").value;
+        // let userID = 
+        // let userEmail = 
+        // let userPass = 
+        // let userStatus = 
+     
         let newPage = document.getElementById("submit");
+        emailStatus = await checkEmail(inputEmail)
+        passwordStatus = await passwordMatches(inputEmail, inputPass)
+        activeStatus = await isActiveUser(inputEmail)
 
-        if(users[username] == password){
-            newPage.setAttribute("href", "index.html");
+        // Check if email exists
+        if(emailStatus === false){
+            console.log("Email failed")
+            loginFailed();
+            return;
         }
-        // If password doesn't match, alert user of sign-in failure
+        // Check if password matches email
+        else if(passwordStatus === false){
+            console.log("password failed")
+            loginFailed();
+            return;
+        }
+        // Check if user is blocked
+        else if(activeStatus === false){
+            console.log("active failed")
+            loginFailed();
+            return;
+        }
+        
+        // **** BAD CONVENTION ****
+        // on my local system, I renamed login.html to index.html
+        // and then I changed index.html to index1.html
+        // ************************
         else {
-            newPage.setAttribute("href", "login.html");
+            //alert("SUCCESS")
+            newPage.setAttribute("href", "index1.html");
         }
+}
+
+function loginFailed(){
+    alert("INCORRECT LOGIN INFORMATION\nPlease try again")
+    let newPage = document.getElementById("submit");
+    newPage.setAttribute("href", "index.html");
+}
+
+async function checkEmail(inputEmail) {
+    let get_user_record = {tablename: 'user_table', 
+                           column_name: 'email',
+                           value: inputEmail};
+
+    let result = await apiGetRecord(get_user_record);
+    console.log(result)
+    console.log(result.length)
+
+    if(result.length === 0){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+async function passwordMatches(inputEmail, inputPass) {
+    let get_user_record = {tablename: 'user_table', 
+                           column_name: 'email',
+                           column_name: 'password',
+                           value: inputEmail, 
+                           value: inputPass  };
+
+    let result = await apiGetRecord(get_user_record);
+    console.log(result)
+
+    if(result.length === 0){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+async function isActiveUser(inputEmail) {
+    let get_user_record = {tablename: 'user_table', 
+                           column_name: 'email',
+                           column_name: 'status',
+                           value: inputEmail, 
+                           value: 'active'  };
+
+    let result = await apiGetRecord(get_user_record);
+    console.log(result)
+
+    if(result.length === 0){
+        return false;
+    }
+    else {
+        return true;
+    }
 }
